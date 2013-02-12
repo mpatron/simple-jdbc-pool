952e43d64e0519eaac24521b914afcf8ae92fbcf
Mickael Patron <mickael_patron@hotmail.com>
2013-02-12 01:37:40 +0100

    Test du delay

--- src/test/java/org/jobjects/jdbc/pool/simple/JDCConnectionPoolTest.java
+++ src/test/java/org/jobjects/jdbc/pool/simple/JDCConnectionPoolTest.java
@@ -1,41 +1,137 @@
 package org.jobjects.jdbc.pool.simple;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JDCConnectionPoolTest {
 
-  @Test
-  public void JDCConnectionPool() {
-    throw new RuntimeException("Test not implemented");
-  }
+	private static Logger LOGGER = Logger.getLogger(JDCConnectionPoolTest.class
+			.getName());
 
-  @Test
-  public void closeConnections() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void JDCConnectionPool() {
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 60000;
+		long delay = 300000;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
+			Assert.assertNotNull(jpool);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
 
-  @Test
-  public void getConnection() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void closeConnections() {
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 60000;
+		long delay = 300000;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
+			jpool.closeConnections();
+			Assert.assertNotNull(jpool);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
 
-  @Test
-  public void getConnectionInUse() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void getConnection() {
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 60000;
+		long delay = 300000;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
 
-  @Test
-  public void reapConnections() {
-    throw new RuntimeException("Test not implemented");
-  }
+			Connection connection = jpool.getConnection();
+			Assert.assertNotNull(connection);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
 
-  @Test
-  public void removeConnection() {
-    throw new RuntimeException("Test not implemented");
-  }
+	@Test
+	public void getConnectionInUse() {
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 60000;
+		long delay = 300000;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
 
-  @Test
-  public void returnConnection() {
-    throw new RuntimeException("Test not implemented");
-  }
+			Connection connection = jpool.getConnection();
+			Assert.assertNotNull(connection);
+			Assert.assertTrue(jpool.getConnectionInUse() > 0);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
+
+	@Test
+	public void reapConnections() {
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 500;
+		long delay = 300;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
+
+			Connection connection = jpool.getConnection();
+			Assert.assertNotNull(connection);
+			Thread.sleep(600);
+			connection.close();
+			jpool.reapConnections();
+			Assert.assertTrue(jpool.getConnectionInUse() == 0);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
+
+	@Test
+	public void returnConnection() {
+		String url = "jdbc:derby:memory:MyDerbyDB;upgrade=true";
+		String user = "sa";
+		String password = "manager";
+		long timeout = 500;
+		long delay = 300000;
+		try {
+			JDCConnectionPool jpool = new JDCConnectionPool(url, user,
+					password, timeout, delay);
+
+			Connection connection = jpool.getConnection();
+			Assert.assertNotNull(connection);
+			Thread.sleep(600);
+			connection.close();
+			jpool.returnConnection((JDCConnection)connection);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, "Erreur non prévu", e);
+			Assert.assertFalse(true);
+		}
+	}
+
 }
